a7554b0c6294778b10af30da3ef3fbfd9d8afae1
Ilia Dulkin <70949446+ded0inside@users.noreply.github.com>
2024-08-19 16:04:09 +0300

    Update subtitle_creator.py
    Исправил 2с баг и заставил текст заходить за рамки экрана полностью

--- it_solutions_test_task/main/subtitle_creator.py
+++ it_solutions_test_task/main/subtitle_creator.py
@@ -3,8 +3,9 @@ from moviepy.editor import ImageSequenceClip
 import numpy as np
 import os
 
+
 # Настройки
-TEXT = "Длинный текст супер длинный текст"
+TEXT = "1234567"
 FONT_SIZE = 70
 IMAGE_WIDTH = 100
 IMAGE_HEIGHT = 100
@@ -21,16 +22,15 @@ def create_frame(text, offset, background_color, font_color, font_size):
     draw = ImageDraw.Draw(image)
 
     # Настройка шрифт
-    # font = ImageFont.truetype("Arial", font_size)  # Для обычного запуска раскоментируете эту строчку и закоментируите следущую
-    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", font_size)  # Нужно для колаба
-    
+    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", font_size)
+    # font = ImageFont.truetype("Arial", font_size)  # Нужно для дебагга 
+
     # Вычисление ширины текста
     bbox = draw.textbbox((0, 0), text, font=font)
-    # text_width = bbox[2] - bbox[0]  # Пока не используется, пофиксим в будущем
     text_height = bbox[3] - bbox[1]
 
     # Позиция якоря для текста
-    x = IMAGE_WIDTH / 2 + offset
+    x = offset
     y = IMAGE_HEIGHT / 2 - text_height / 2
 
     # Отрисовка текста на изображении
@@ -39,32 +39,37 @@ def create_frame(text, offset, background_color, font_color, font_size):
     return np.array(image)
 
 
-def create_subtitle_video(text, font="Arial", font_size=FONT_SIZE, font_color=(255, 255, 255),
+def create_subtitle_video(text, font_size=FONT_SIZE, font_color=(255, 255, 255),
                           background_color=(0, 0, 0)):
     '''Создает видео с бегущей строкой, возвращает путь до файла в папке media'''
     global IMAGE_WIDTH, IMAGE_HEIGHT, FPS
 
     # Настройка шрифт
-    # font = ImageFont.truetype("Arial", font_size)  # Для обычного запуска раскоментируете эту строчку и закоментируите следущую
-    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", font_size)  # Нужно для колаба
-                              
+    font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", font_size)
+    # font = ImageFont.truetype("Arial", font_size)  # Нужно для дебагга
+
     # Создание одного кадра для получения ширины текста
     bbox = ImageDraw.Draw(Image.new('RGB', (IMAGE_WIDTH, IMAGE_HEIGHT))).textbbox((0, 0), text,
                                                                                   font=font)
     # Создание служебных переменных
     frames = []  # Массив кадров для видео
-    # offset = IMAGE_WIDTH  # Начальная позиция текста за пределами экрана
-    offset = 0  # Начальная позиция текста если нужно сразу выводить текст на экран
+    offset = IMAGE_WIDTH  # Начальная позиция текста за пределами экрана
+    # offset = 0  # Начальная позиция текста если нужно сразу выводить текст на экран
     text_width = bbox[2] - bbox[0]  # Длина текста в пикселях
-    text_speed = (text_width + IMAGE_WIDTH) // (FPS * 3)  # Скорость смещения (пикселей/кадр), чтобы уложиться в 3с
-
+    text_speed = round((text_width + IMAGE_WIDTH) / (FPS * 3))  # Скорость смещения (пикселей/кадр), чтобы уложиться в 3с
+    print(text_width)
+    print(text_speed)
     while True:
         frame = create_frame(text, offset, background_color, font_color, font_size)
         frames.append(frame)
         offset -= text_speed  # Изменение смещения текста
 
-        if -offset >= text_width:  # Выходим из цикла, когда весь текст отрисован
+        if -offset > text_width:  # Выходим из цикла, когда весь текст отрисован
             break
+
+    media_dir = "media"
+    if not os.path.exists(media_dir):
+        os.makedirs(media_dir)
 
     # Путь для записи файла
     output_path = os.path.join("media", "subtitle_video.mp4")
